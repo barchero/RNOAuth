ff9d937b097baf55be03842145e2a0ba3a82d27c
Christian Bartrina <christian.bartrina@cabsa.es>
2020-02-25 11:48:19 +0100

    Solucionado problema con fuente Roboto en dispositivos android

--- App.tsx
+++ App.tsx
@@ -2,12 +2,29 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import {store} from './app/store/store';
 import {Login} from './app/views/auth/Login';
+import * as Font from 'expo-font';
 
-export default function App() {
+export default class App extends React.Component {
+    state = {
+        fontLoaded: false,
+    };
 
-  return (
-      <Provider store={store}>
-        <Login/>
-      </Provider>
-  );
+    async componentWillMount() {
+        await Font.loadAsync({
+            'Roboto_medium': require('./assets/fonts/Roboto/Roboto-Medium.ttf'),
+        });
+        this.setState({ fontLoaded: true });
+    }
+
+    render() {
+        return (
+            <Provider store={store}>
+                {
+                    this.state.fontLoaded ? (
+                        <Login/>
+                    ) : null
+                }
+            </Provider>
+        );
+    }
 }
